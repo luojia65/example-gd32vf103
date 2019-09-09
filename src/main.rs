6f4007afcc267ad0fdb8cec3b899fcc79e3d35b8
luojia65 <me@luojia.cc>
2019-09-09 20:54:20 +0800

    Test

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@
 extern crate panic_halt;
 
 use riscv_rt::entry;
-use gd32vf103_pac as pac;
+// use gd32vf103_pac as pac;
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    dp.RCU.apb2en.write(|w| w.pcen().set_bit());
-    dp.GPIOC.ctl1.write(|w| unsafe {
-        w.ctl13().bits(0b00).md13().bits(0b11)
-    });
-    loop {
-        dp.GPIOC.bop.write(|w| w.bop13().set_bit());
-        dp.GPIOC.bc.write(|w| w.cr13().set_bit());
+    let gpioa_ctl0 = 0x4001_0800_usize as *mut u32;
+    let gpioc_ctl1 = 0x4001_1004_usize as *mut u32;
+    let gpioa_bop = 0x4001_0810_usize as *mut u32;
+    let gpioa_bc = 0x4001_0814_usize as *mut u32;
+    let gpioc_bop = 0x4001_1010_usize as *mut u32;
+    let gpioc_bc = 0x4001_1014_usize as *mut u32;
+    let rcu_apb2en = 0x4002_1018_usize as *mut u32;
+    unsafe {
+        *rcu_apb2en = 0x00000014_u32;
+        *gpioa_ctl0 = 0x44444334_u32;
+        *gpioc_ctl1 = 0x44344444_u32;
+        *gpioa_bop = 0b00000000_00000000_00000000_00000110_u32;
+        *gpioa_bc  = 0b00000000_00000000_00000000_00000010_u32;
+        *gpioc_bop = 0b00000000_00000000_00100000_00000000_u32;
+        *gpioc_bc  = 0b00000000_00000000_00100000_00000000_u32;
     }
+    loop {}
 }
