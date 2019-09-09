08528922bb869300fe8c6fea540a907c454ea43d
luojia65 <me@luojia.cc>
2019-09-09 21:02:23 +0800

    Green light!

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,14 @@
 extern crate panic_halt;
 
 use riscv_rt::entry;
-// use gd32vf103_pac as pac;
+use gd32vf103_pac as pac;
 
 #[entry]
 fn main() -> ! {
-    let gpioa_ctl0 = 0x4001_0800_usize as *mut u32;
-    let gpioc_ctl1 = 0x4001_1004_usize as *mut u32;
-    let gpioa_bop = 0x4001_0810_usize as *mut u32;
-    let gpioa_bc = 0x4001_0814_usize as *mut u32;
-    let gpioc_bop = 0x4001_1010_usize as *mut u32;
-    let gpioc_bc = 0x4001_1014_usize as *mut u32;
-    let rcu_apb2en = 0x4002_1018_usize as *mut u32;
-    unsafe {
-        *rcu_apb2en = 0x00000014_u32;
-        *gpioa_ctl0 = 0x44444334_u32;
-        *gpioc_ctl1 = 0x44344444_u32;
-        *gpioa_bop = 0b00000000_00000000_00000000_00000110_u32;
-        *gpioa_bc  = 0b00000000_00000000_00000000_00000010_u32;
-        *gpioc_bop = 0b00000000_00000000_00100000_00000000_u32;
-        *gpioc_bc  = 0b00000000_00000000_00100000_00000000_u32;
-    }
+    let dp = pac::Peripherals::take().unwrap();
+    dp.RCU.apb2en.write(|w| w.paen().set_bit());
+    dp.GPIOA.bop.write(|w| unsafe { w.bits(0x0000FFFF) });
+    dp.GPIOA.ctl0.write(|w| unsafe { w.md1().bits(0b11).ctl1().bits(0b00) });
+    dp.GPIOA.bc.write(|w| w.cr1().set_bit());
     loop {}
 }
