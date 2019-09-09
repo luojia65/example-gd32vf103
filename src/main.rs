0b6ff618ccc63a5226230fb9aeacc61b3c99eccd
luojia65 <me@luojia.cc>
2019-09-09 21:51:28 +0800

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,8 @@ use gd32vf103_hal::pac as pac;
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    dp.RCU.apb2en.write(|w| w.paen().set_bit());
     let mut gpioa = dp.GPIOA.split();
     let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
     pa1.set_low().unwrap();
-    // dp.GPIOA.bop.write(|w| unsafe { w.bits(0x0000FFFF) });
-    // dp.GPIOA.ctl0.write(|w| unsafe { w.md1().bits(0b11).ctl1().bits(0b00) });
-    // dp.GPIOA.bc.write(|w| w.cr1().set_bit());
     loop {}
 }
