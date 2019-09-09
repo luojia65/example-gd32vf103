ec565695ef09664b356ca4f3fec18758741f88bb
luojia65 <me@luojia.cc>
2019-09-09 22:17:29 +0800

    Rcu constrain

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ extern crate panic_halt;
 use riscv_rt::entry;
 use gd32vf103_hal as hal;
 use hal::prelude::*;
-use gd32vf103_hal::pac as pac;
+use hal::pac as pac;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    let mut gpioa = dp.GPIOA.split();
+    let mut rcu = dp.RCU.constrain();
+    let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
     let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
     pa1.set_low().unwrap();
     loop {}
